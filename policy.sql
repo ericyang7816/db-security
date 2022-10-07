c8eb553521c9306415da91528ab0b90efbf42b30
Eric Yang <55185008+ericyang7816@users.noreply.github.com>
2022-10-07 16:46:02 +0800

    ver1 policy completed

--- policy.sql
+++ policy.sql
@@ -1,26 +1,11 @@
-/*
 CREATE OR REPLACE
 FUNCTION show_own_salary(v_schema IN VARCHAR2, v_obj IN VARCHAR2)
 RETURN VARCHAR2 AS condition VARCHAR2 (200);
 BEGIN
-IF SYS_CONTEXT('SYS_SESSION_ROLES', 'DBA') = 'TRUE' THEN
+IF INSTR(SYS_CONTEXT('USERENV', 'SESSION_USER'),'COMPANY') = 1 THEN
 		RETURN '';
 END IF;
-IF SYS_CONTEXT( 'SYS_SESSION_ROLES', 'ALL_ACCOUNTANT' ) = 'TRUE' THEN
-    RETURN '';
-END IF;
-RETURN 'SID = (SYS_CONTEXT(''USERENV'', ''SESSION_USER'' ))';
-END show_own_salary;
-*/
-
-CREATE OR REPLACE
-FUNCTION show_own_salary(v_schema IN VARCHAR2, v_obj IN VARCHAR2)
-RETURN VARCHAR2 AS condition VARCHAR2 (200);
-BEGIN
-IF SYS_CONTEXT('USERENV', 'SESSION_USER')  = 'COMPANY' THEN
-		RETURN '';
-END IF;
-IF SYS_CONTEXT( 'USERENV', 'SESSION_USER' ) = 'ACCOUNTANT' THEN
+IF INSTR(SYS_CONTEXT( 'USERENV', 'SESSION_USER' ),'ACCOUNTANT') = 1 THEN
     RETURN '';
 END IF;
 RETURN 'SID = (SYS_CONTEXT(''USERENV'', ''SESSION_USER'' ))';
@@ -45,10 +30,10 @@ CREATE OR REPLACE
 FUNCTION show_own_attendance(v_schema IN VARCHAR2, v_obj IN VARCHAR2)
 RETURN VARCHAR2 AS condition VARCHAR2 (200);
 BEGIN
-IF SYS_CONTEXT('USERENV', 'SESSION_USER')  = 'COMPANY' THEN
+IF INSTR(SYS_CONTEXT('USERENV', 'SESSION_USER'),'COMPANY') = 1 THEN
 		RETURN '';
 END IF;
-IF SYS_CONTEXT( 'USERENV', 'SESSION_USER' ) = 'HR' THEN
+IF INSTR(SYS_CONTEXT('USERENV', 'SESSION_USER'),'HR') = 1 THEN
     RETURN '';
 END IF;
 RETURN 'SID = (SYS_CONTEXT(''USERENV'', ''SESSION_USER'' ))';
@@ -73,10 +58,13 @@ CREATE OR REPLACE
 FUNCTION show_own_info(v_schema IN VARCHAR2, v_obj IN VARCHAR2)
 RETURN VARCHAR2 AS condition VARCHAR2 (200);
 BEGIN
-IF SYS_CONTEXT('USERENV', 'SESSION_USER')  = 'COMPANY' THEN
+IF INSTR(SYS_CONTEXT('USERENV', 'SESSION_USER'),'COMPANY') = 1 THEN
 		RETURN '';
 END IF;
-IF SYS_CONTEXT( 'USERENV', 'SESSION_USER' ) = 'HR' THEN
+IF INSTR(SYS_CONTEXT('USERENV', 'SESSION_USER'),'HR') = 1 THEN
+    RETURN '';
+END IF;
+IF INSTR(SYS_CONTEXT('USERENV', 'SESSION_USER'),'ACCOUNTANT') = 1 THEN
     RETURN '';
 END IF;
 RETURN 'SID = (SYS_CONTEXT(''USERENV'', ''SESSION_USER'' ))';
@@ -88,7 +76,8 @@ BEGIN
 		object_name	=>	'staff',
 		policy_name	=>	'show_own_info_policy',
 		policy_function	=>	'show_own_info',
-		sec_relevant_cols=> 'DOB');
+		sec_relevant_cols=> 'ACCOUNT,SECRET',
+		update_check => TRUE);
 END;
 
 BEGIN
@@ -98,13 +87,128 @@ BEGIN
 		policy_name	=>	'show_own_info_policy');
 END;
 
+CREATE OR REPLACE
+FUNCTION show_own_interview(v_schema IN VARCHAR2, v_obj IN VARCHAR2)
+RETURN VARCHAR2 AS condition VARCHAR2 (200);
+BEGIN
+IF INSTR(SYS_CONTEXT('USERENV', 'SESSION_USER'),'COMPANY') = 1 THEN
+		RETURN '';
+END IF;
+IF INSTR(SYS_CONTEXT('USERENV', 'SESSION_USER'),'HR') = 1 THEN
+    RETURN '';
+END IF;
+RETURN 'INTERVIEWER = (SYS_CONTEXT(''USERENV'', ''SESSION_USER'' ))';
+END show_own_interview;
 
+BEGIN
+	DBMS_RLS.ADD_POLICY (
+		object_schema	=>	'company',
+		object_name	=>	'interview',
+		policy_name	=>	'show_own_interview_policy',
+		policy_function	=>	'show_own_interview',
+		update_check => TRUE);
+END;
+
+BEGIN
+	DBMS_RLS.DROP_POLICY (
+		object_schema	=>	'company',
+		object_name	=>	'staff',
+		policy_name	=>	'show_own_info_policy');
+END;
+
+CREATE OR REPLACE
+FUNCTION show_secret_project(v_schema IN VARCHAR2, v_obj IN VARCHAR2)
+RETURN VARCHAR2 ASsecret number (10,2);
+condition VARCHAR2 (200);
+BEGIN
+IF INSTR(SYS_CONTEXT('USERENV', 'SESSION_USER'),'COMPANY') = 1 THEN
+		RETURN '';
+END IF;
+SELECT SECRET INTO secret from staff where sid = (SYS_CONTEXT('USERENV', 'SESSION_USER' ));
+RETURN 'SECRET <= ' || secret;
+END show_secret_project;
+
+BEGIN
+	DBMS_RLS.ADD_POLICY (
+		object_schema	=>	'company',
+		object_name	=>	'project',
+		policy_name	=>	'show_secret_project_policy',
+		policy_function	=>	'show_secret_project',
+		update_check => TRUE);
+END;
+
+BEGIN
+	DBMS_RLS.DROP_POLICY (
+		object_schema	=>	'company',
+		object_name	=>	'project',
+		policy_name	=>	'show_secret_project_policy');
+END;
+
+CREATE OR REPLACE
+FUNCTION show_projectgroup(v_schema IN VARCHAR2, v_obj IN VARCHAR2)
+RETURN VARCHAR2 AS
+condition VARCHAR2 (200);
+BEGIN
+IF INSTR(SYS_CONTEXT('USERENV', 'SESSION_USER'),'COMPANY') = 1 THEN
+		RETURN '';
+END IF;
+IF INSTR(SYS_CONTEXT('USERENV', 'SESSION_USER'),'HR') = 1 THEN
+		RETURN '';
+END IF;
+RETURN 'PID IN (SELECT PID FROM PROJECT WHERE LEADER = (SYS_CONTEXT(''USERENV'', ''SESSION_USER'' ))) OR PARTICIPANT = (SYS_CONTEXT(''USERENV'', ''SESSION_USER'' ))';
+END show_projectgroup;
+
+BEGIN
+	DBMS_RLS.ADD_POLICY (
+		object_schema	=>	'company',
+		object_name	=>	'projectgroup',
+		policy_name	=>	'show_projectgroup_policy',
+		policy_function	=>	'show_projectgroup',
+		statement_types => 'SELECT',
+		update_check => TRUE);
+END;
+
+BEGIN
+	DBMS_RLS.DROP_POLICY (
+		object_schema	=>	'company',
+		object_name	=>	'projectgroup',
+		policy_name	=>	'show_projectgroup_policy');
+END;
+
+CREATE OR REPLACE
+FUNCTION update_projectgroup(v_schema IN VARCHAR2, v_obj IN VARCHAR2)
+RETURN VARCHAR2 AS
+condition VARCHAR2 (200);
+BEGIN
+IF INSTR(SYS_CONTEXT('USERENV', 'SESSION_USER'),'COMPANY') = 1 THEN
+		RETURN '';
+END IF;
+RETURN 'PID IN (SELECT PID FROM PROJECT WHERE LEADER = (SYS_CONTEXT(''USERENV'', ''SESSION_USER'' )))';
+END update_projectgroup;
+
+BEGIN
+	DBMS_RLS.ADD_POLICY (
+		object_schema	=>	'company',
+		object_name	=>	'projectgroup',
+		policy_name	=>	'update_projectgroup_policy',
+		policy_function	=>	'update_projectgroup',
+		statement_types => 'UPDATE',
+		update_check => TRUE);
+END;
+
+BEGIN
+	DBMS_RLS.DROP_POLICY (
+		object_schema	=>	'company',
+		object_name	=>	'projectgroup',
+		policy_name	=>	'update_projectgroup');
+END;
 
 /*
 ALTER USER COMPANY DEFAULT ROLE DBA;
 select SID from STAFF; where SID = (SYS_CONTEXT('USERENV', 'SESSION_USER' ))
 select (SYS_CONTEXT('USERENV', 'ISDBA' )) from dual
 SELECT SYS_CONTEXT('SYS_SESSION_ROLES', 'DBA') FROM DUAL;
+select SYS_CONTEXT( 'SYS_SESSION_ROLES', 'ALL_ACCOUNTANT' ) from dual
 	SELECT * FROM session_roles;
 	SELECT * FROM ALL_POLICIES
 SELECT * FROM USER_SYS_PRIVS; SELECT * FROM USER_TAB_PRIVS; SELECT * FROM USER_ROLE_PRIVS;
--- user.sql
+++ user.sql
@@ -1,7 +1,7 @@
 create role all_hr not identified;
 create role all_accountant not identified;
 create role all_staff not identified;
-
+create role all_applicant not identified;
 
 grant connect to all_hr;
 grant select,update on staff to all_hr;
@@ -10,18 +10,17 @@ grant select,update,insert,delete on attendance to all_hr;
 grant select,update,insert,delete on interview to all_hr;
 grant select on salary to all_hr;
 grant select on position to all_hr;
+grant select on projectgroup to all_hr;
 
 grant connect to all_accountant;
 grant select on staff to all_accountant;
-grant select on hire to all_hr; 
 grant select,update,insert on salary to all_accountant;
 grant select on attendance to all_accountant;
 grant select on position to all_accountant; 
 
 grant connect to all_staff;
 grant select,update on staff to all_staff;
-grant select on hire to all_staff;
-grant select,update on interview to all_hr;
+grant select,update on interview to all_staff;
 grant select on salary to all_staff;grant select on project to all_staff;
 grant select,update on projectgroup to all_staff;
 grant select on attendance to all_staff; 
@@ -35,9 +34,12 @@ create user staff1 identified by 123;
 grant all_staff to staff1;
 create user staff2 identified by 123;
 grant all_staff to staff2;
-
 create user applicant identified by 123;
-grant select on hire to applicant;
+grant all_applicant to applicant;
+
+grant select on hire_public to all_accountant;
+grant select on hire_public to all_staff;
+grant select on hire_public to applicant;
 
 /*
 grant connect to hr;
@@ -50,6 +52,7 @@ drop role all_accountant;
 drop role all_staff;
 
 DROP USER staff1;
+DROP USER staff2;
 DROP USER hr;
 DROP USER accountant;
 DROP USER applicant;
