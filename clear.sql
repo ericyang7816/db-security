e16ffeaab9ac8712c336c565f0e5034090c9551d
Eric Yang <55185008+ericyang7816@users.noreply.github.com>
2022-11-01 23:54:39 +0800

    建表

--- clear.sql
+++ clear.sql
@@ -16,34 +16,8 @@ drop table PROJECTGROUP cascade constraints
 drop table PROJECT cascade constraints
 /
 
-drop function SHOW_OWN_SALARY
-/
-
-drop function SHOW_OWN_ATTENDANCE
-/
-
-drop function SHOW_OWN_INFO
-/
-
-drop function SHOW_OWN_INTERVIEW
-/
-
-drop function SHOW_SECRET_PROJECT
-/
-
 drop table STAFF cascade constraints
 /
 
 drop table POSITION cascade constraints
 /
-
-drop function SHOW_PROJECTGROUP
-/
-
-drop function UPDATE_PROJECTGROUP
-/
-
-drop function SHOW_OWN_POSITION_BASE_SALARY
-/
-
-
